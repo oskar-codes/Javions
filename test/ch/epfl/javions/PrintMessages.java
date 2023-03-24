df70ebbc2e5b2a9108ac78754b33a25f8d808bc1
Oskar <oskar.codes@gmail.com>
2023-03-24 18:22:23 +0100

    Step 6 started

--- test/ch/epfl/javions/PrintMessages.java
+++ test/ch/epfl/javions/PrintMessages.java
@@ -1,6 +1,7 @@
 package ch.epfl.javions;
 
 import ch.epfl.javions.adsb.AirbornePositionMessage;
+import ch.epfl.javions.adsb.AirborneVelocityMessage;
 import ch.epfl.javions.adsb.AircraftIdentificationMessage;
 import ch.epfl.javions.adsb.RawMessage;
 import ch.epfl.javions.demodulation.AdsbDemodulator;
@@ -17,41 +18,59 @@ public final class PrintMessages {
         System.out.println("Started");
         Date start = new Date();
 
-        System.out.println("### Q = 0 ###");
-        RawMessage messageA = new RawMessage(0, ByteString.ofHexadecimalString("8D39203559B225F07550ADBE328F"));
-        AirbornePositionMessage airbornePositionMessageA = AirbornePositionMessage.of(messageA);
-        System.out.println(airbornePositionMessageA);
+        int identification = 0;
+        int position = 0;
+        int velocity = 0;
+        int ignored = 0;
 
-        RawMessage messageB = new RawMessage(0, ByteString.ofHexadecimalString("8DAE02C85864A5F5DD4975A1A3F5"));
-        AirbornePositionMessage airbornePositionMessageB = AirbornePositionMessage.of(messageB);
-        System.out.println(airbornePositionMessageB);
-
-        System.out.println("### Q = 1 ###");
+        double maxSpeed = -1;
+        double minSpeed = Double.MAX_VALUE;
 
         String f = "resources/samples_20230304_1442.bin";
         try (InputStream s = new FileInputStream(f)) {
             AdsbDemodulator d = new AdsbDemodulator(s);
             RawMessage m;
-            int n = 0;
 
             while ((m = d.nextMessage()) != null) {
                 if (m.typeCode() >= 1 && m.typeCode() <= 4) {
                     AircraftIdentificationMessage a = AircraftIdentificationMessage.of(m);
                     System.out.println(a);
-                    n++;
+                    identification++;
+                     continue;
                 }
-
-//                if (m.typeCode() >= 9 && m.typeCode() <= 18 || m.typeCode() >= 20 && m.typeCode() <= 22) {
-//                    AirbornePositionMessage a = AirbornePositionMessage.of(m);
-//                    System.out.println(a);
-//                    n++;
-//                }
+                if (m.typeCode() >= 9 && m.typeCode() <= 18 || m.typeCode() >= 20 && m.typeCode() <= 22) {
+                    AirbornePositionMessage a = AirbornePositionMessage.of(m);
+                    System.out.println(a);
+                    position++;
+                     continue;
+                }
+                if (m.typeCode() == 19) {
+                    AirborneVelocityMessage a = AirborneVelocityMessage.of(m);
+                    if (a != null && a.speed() > maxSpeed) {
+                        maxSpeed = a.speed();
+                    }
+                     if (a != null && a.speed() < minSpeed) {
+                           minSpeed = a.speed();
+                     }
+                    System.out.println(a);
+                    velocity++;
+                    continue;
+                }
+                ignored++;
             }
-
-            System.out.println("Number of messages: " + n);
         }
 
         Date end = new Date();
-        System.out.println("Time spent: " + (end.getTime() - start.getTime()) / 1000.0 + " seconds");
+        System.out.println();
+        System.out.println("Executed in: " + (end.getTime() - start.getTime()) / 1000.0 + " seconds");
+        System.out.println();
+        System.out.println("Max speed: " + maxSpeed * 3.6 + " km/h");
+        System.out.println("Min speed: " + minSpeed * 3.6 + " km/h");
+        System.out.println();
+        System.out.println("Identification messages: " + identification);
+        System.out.println("Position messages: " + position);
+        System.out.println("Velocity messages: " + velocity);
+        System.out.println("Ignored messages: " + ignored);
+        System.out.println("Number of messages: " + (identification + position + velocity + ignored));
     }
 }
---
+++ test/ch/epfl/javions/adsb/AirborneVelocityMessage.java
@@ -0,0 +1,54 @@
+package ch.epfl.javions.adsb;
+
+import ch.epfl.javions.Bits;
+import ch.epfl.javions.Units;
+import ch.epfl.javions.aircraft.IcaoAddress;
+
+import static ch.epfl.javions.Preconditions.checkArgument;
+
+public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress, double speed, double trackOrHeading) {
+    public AirborneVelocityMessage {
+        if (icaoAddress == null) throw new NullPointerException("ICAO address is null");
+        checkArgument(timeStampNs >= 0 && speed >= 0 && trackOrHeading >= 0);
+    }
+
+    public static AirborneVelocityMessage of(RawMessage rawMessage) {
+        // TODO: Check if this is correct
+        int subtype = Bits.extractUInt(rawMessage.payload(), 48, 3);
+        int data = Bits.extractUInt(rawMessage.payload(), 21, 22);
+
+        if (subtype == 1 || subtype == 2) {
+            int dns = Bits.extractUInt(data, 10, 1);
+            int vns = Bits.extractUInt(data, 0, 10) - 1;
+
+            int dew = Bits.extractUInt(data, 21, 1);
+            int vew = Bits.extractUInt(data, 11, 10) - 1;
+
+            if (vns == -1 || vew == -1) return null;
+
+            double speed = Units.convert(Math.hypot(vew, vns) * (subtype == 2 ? 4 : 1), Units.Speed.KNOT, Units.Speed.KILOMETER_PER_HOUR) * 1000 / 3600;
+
+            if (dns == 1) vns = -vns;
+            if (dew == 1) vew = -vew;
+
+            double heading = Math.atan2(vew, vns);
+            if (heading < 0) heading += 2 * Math.PI;
+
+            return new AirborneVelocityMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), speed, heading);
+        }
+
+        if (subtype == 3 || subtype == 4) {
+            boolean sh = Bits.testBit(data, 21);
+            if (sh) {
+                long hdg = Integer.toUnsignedLong(Bits.extractUInt(data, 11, 10));
+                double result = Units.convertFrom(Math.scalb(hdg, -10), Units.Angle.TURN);
+
+                int speed = Bits.extractUInt(data, 0, 10) * (subtype == 4 ? 4 : 1);
+
+                return new AirborneVelocityMessage(rawMessage.timeStampNs(), rawMessage.icaoAddress(), speed, result);
+            }
+        }
+
+        return null;
+    }
+}
